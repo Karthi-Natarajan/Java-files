023b047b6d30167237a897a72d09cb4500aeac4e
Karthi~Natarajan <136811565+Karthi-Natarajan@users.noreply.github.com>
2025-03-01 16:10:45 +0530

    Add files via upload

---
+++ Largestarraynum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class Largestarraynum{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the array number: ");
+        int n = sc.nextInt();
+        
+        int[] arr= new int[n];
+        System.out.print("Enter "+n+" elements :");
+        
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        int max = arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println("The largest number in an array : "+max);
+        sc.close();
+    }
+}
+        
+   
