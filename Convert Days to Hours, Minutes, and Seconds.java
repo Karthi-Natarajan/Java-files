d7e503e7e8ce8a47cc581cb438e22dfe04e2ac0d
Karthi~Natarajan <136811565+Karthi-Natarajan@users.noreply.github.com>
2025-03-05 15:23:06 +0530

    Update Convert Days to Hours, Minutes, and Seconds.java

--- Convert Days to Hours, Minutes, and Seconds.java
+++ Convert Days to Hours, Minutes, and Seconds.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 public class TimeConversion {
+    
+    public static int multiply(int num, int factor) {
+        int result = 0;
+        for (int i = 0; i < factor; i++) {
+            result += num;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.print("Enter number of days: ");
         int days = scanner.nextInt();
         
-        int hours = days * 24;
-        int minutes = hours * 60;
-        int seconds = minutes * 60;
+    
+        int hours = multiply(days, 24);
+        int minutes = multiply(hours, 60);
+        int seconds = multiply(minutes, 60);
         
         System.out.println(days + " days = " + hours + " hours = " + minutes + " minutes = " + seconds + " seconds");
+        
         scanner.close();
     }
 }
+
